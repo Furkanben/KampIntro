1ac70a53a7f6014b8a0b326f91915f352f44a74e
furkan bolat <furkan.bolatkale@hotmail.com>
2021-01-17 20:33:24 +0300

    Deneme17.1-2

--- ClassIntro/Program.cs
+++ ClassIntro/Program.cs
@@ -11,7 +11,7 @@ namespace ClassIntro
 
             Kurs kurs1=new Kurs();
             kurs1.KursAdi = "C#";
-            kurs1.Egitmen = "Engin D.";
+            kurs1.Egitmen = "Engin Demir";
             kurs1.IzlenmeOrani = 68;
 
             Kurs kurs2 = new Kurs();
