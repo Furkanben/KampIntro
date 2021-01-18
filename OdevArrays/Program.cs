3a4b589238c47b88a605aeaa9da78830924d73ac
Ben Furkan <furkan.bolatkale@hotmail.com>
2021-01-18 22:45:11 +0300

    3. Ders 1. Ödev: Sırasıyla OdevMethods, OdevRefKeyword, OdevOutKeyword, OdevMethodOverloading, OdevParamsKeyword, OdevArrays çalışılacak şekilde düzenlendi.

---
+++ OdevArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdevArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string student1 = "Metin";
+            //string student2 = "Ali";
+            //string student3 = "Feyyaz";
+            //Bu şekilde tek tek yazmak yerine Array tanımlanır:
+            //string[] students = new string[3];
+            //students[0] = "Metin"; //students adlı array'in sıfırıncı (ilk) öğesi Metin.
+            //students[1] = "Ali"; //students adlı array'in 1. öğesi (0'dan başladığı için aslında ikinci) öğesi Ali.
+            //students[2] = "Feyyaz"; //students adlı array'in 2. (0'dan başladığı için aslında üçüncü) öğesi MetiFeyyaz.
+
+            //ANCAK BUNUN EN KOLAY YOLU:
+
+            string[] students = new[] { "Metin", "Ali", "Feyyaz" }; //Burada {} içine isteğe göre öğe eklenebilir (dinamik). NOT: new[] ifadesi çıkarılabilir (string[] students = { "Metin", "Ali", "Feyyaz" }.
+
+            foreach (var student in students) //students adlı string array içinde her student takma adlı öğe (Metin, Ali, Feyyaz) için dönülmesi tanımlandı.
+            {
+                Console.WriteLine(student);//Her dönüşte sıradaki student takma adlı öğe yazdırılır.
+            }
+        }
+    }
+}
---
+++ OdevMethodOverloading/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OdevMethodOverloading //OdevOutKeyword ÜZERİNDEN DEVAM!
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add();
+            //Add();
+            //Add();
+            //Add();
+            //var result = Add2();
+
+            int number1; //"out" kullanacağımız için "ref"teki gibi "number1" için değer tanımlamamıza gerek yok.
+            int number2 = 100;
+            var result2 = Add3(out number1, number2);//"out" eklendiği için number1 referans tipe dönüştü ve sonradan tanımlanan "30" değerini aldı. "out" olmasaydı number1 değer tip olup yukarıda değer tanımlanmadığı için boşta kalacaktı (hata).
+
+            //Console.WriteLine(result2);
+            //Console.WriteLine(number1);
+
+            Console.WriteLine(Multiply(2,4)); //Method çıktısı (aşağıda tanımlandı).
+            Console.WriteLine(Multiply(2,4,5));//Method overloading çıktısı (aşağıda tanımlandı).
+            Console.ReadLine();
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Added successfully!");
+        }
+
+        static int Add2(int number1 = 20, int number2 = 30)
+        {
+            var result = number1 + number2;
+            return result;
+        }
+        //Eğer sabit rakam ile işlem yapılıyorsa değer tip ile tanımlanan sabit değer kullanılır ancak kullanım durumuna göre sabit rakam yerine değişen (örneğin müşteriye özgü faiz oranı) rakam ile işlem yapılıyorsa "out" keyword'ü ile referans tip kullanılır.("ref" gibi)
+        static int Add3(out int number1, int number2)//"ref" eklendiği için number1 referans tipe dönüştü ve sonradan tanımlanan "30" değerini aldı. "out" olmasaydı number1 değer tip olup yukarıda değer tanımlanmadığı için boşta kalacaktı (hata).
+        {
+            number1 = 30;
+            return number1 + number2;
+        }
+
+        //METHOD
+        static int Multiply(int number1,int number2)//Metodun imzası, yani gerçekleştirilecek işlem tanımlandı, işleme tabi olacak parametreler parantezin içine eklenebilir (Overload edilebilir).
+        {
+            return number1 * number2;
+        }
+
+        //METHOD OVERLOADING
+        static int Multiply(int number1, int number2, int number3)//Metodun imzası, yani gerçekleştirilecek işlem tanımlandı, işleme tabi olacak parametreler parantezin içine eklenebilir (Overload edilebilir).
+        {
+            return number1 * number2 * number3; //Method overloading (üstteki metoda "number3" parametresi eklendi.
+        }
+    }
+}
