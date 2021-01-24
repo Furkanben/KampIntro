9b8f4c9a09c2b145211e12476d0b678da2712aec
Ben Furkan <furkan.bolatkale@hotmail.com>
2021-01-24 22:28:14 +0300

    24.1.21 - Dictionary removed

---
+++ Dictionary/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string StudentNumber { get; set; }
+        public string StudentName { get; set; }
+        public double StudentGrade { get; set; }
+        public string StudentGraduationStatus { get; set; }
+    }
+}
---
+++ Dictionary/StudentManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class StudentManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Displaying student graduation status");
+        }
+
+        public void ShowGrade()
+        {
+            Console.WriteLine("Student grade");
+        }
+    }
+}
