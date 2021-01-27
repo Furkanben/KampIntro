3486cbe51d8dcfcaf4548046379578881537b2b2
Ben Furkan <furkan.bolatkale@hotmail.com>
2021-01-27 23:51:11 +0300

    27.1.21 - SQL added

---
+++ Interfaces/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    //abstract object, doesn't have a function alone
+    interface IPerson//The operation to be implemented to all objects. IPerson contains common properties of Customer and Student.
+    {
+        int Id { get; set; }//Properties that Customer and Student classes must contain.Does not start with "public"
+        int FirstName { get; set; }//Properties that Customer and Student classes must contain.Does not start with "public"
+        int LastName { get; set; }//Properties that Customer and Student classes must contain.Does not start with "public"
+    }
+    //concrete object
+    class Customer : IPerson//Customer (= person) is like a subset of IPerson
+    {
+        public int Id { get ; set ; }//"public" versions of the properties in the IPerson interface
+        public int FirstName { get; set; }//"public" versions of the properties in the IPerson interface
+        public int LastName { get; set; }//"public" versions of the properties in the IPerson interface
+        public int CustomerAddress { get; set; }//customer specific property
+        public int CustomerNumber { get; set; }//customer specific property
+
+    }
+    //concrete object
+    class Student : IPerson//Student (= person) is like a subset of IPerson
+    {
+        public int Id { get; set; }//"public" versions of the properties in the IPerson interface
+        public int FirstName { get; set; }//"public" versions of the properties in the IPerson interface
+        public int LastName { get; set; }//"public" versions of the properties in the IPerson interface
+        public int StudentNumber { get; set; }//student specific property
+        public string Department { get; set; }//student specific property
+    }
+}
---
+++ SQLQuery.sql
@@ -0,0 +1,33 @@
+﻿--Select
+--ANSII and case insensitive
+Select ContactName Adi,CompanyName SirketAdi,City Sehir from Customers
+
+Select * from Customers where City = 'London'
+
+select * from Products where categoryId=1 or CategoryID=3
+
+select * from Products where categoryId=1 and UnitPrice>=10
+
+
+select * from Products where categoryId=1 order by UnitPrice desc --default ascending
+
+
+select count(*) Adet from Products where CategoryID=2
+
+select categoryId,count(*) from Products where unitPrice>20 group by CategoryID having count(*)<10
+
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName
+from products inner join Categories 
+on Products.CategoryID = Categories.CategoryID--if gibi
+where Products.UnitPrice>10
+
+--DTO: Data Transformation Object
+
+Select * from Products p inner join [Order Details] od--Products'ta olup Order Details'te olmayanları da getir
+on p.ProductID = od.ProductID 
+inner join Orders o
+on o.OrderID = od.OrderID
+
+Select * from Customers c left join Orders o
+on c.CustomerID = o.CustomerID
+where o.CustomerID is null--"sana özel" muhabbeti
