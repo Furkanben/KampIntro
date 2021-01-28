2d55c1d18027f6772947643bb5c750d181917a75
Ben Furkan <furkan.bolatkale@hotmail.com>
2021-01-29 00:29:34 +0300

    28.1.21 - ReCap 40-45 added

---
+++ Interfaces/ICustomerDal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface ICustomerDal//Data Access Layer (Dal)
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql updated");
+        }
+    }
+
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle updated");
+        }
+    }
+
+    class CustomerManager
+    {
+        public void Add(ICustomerDal customerDal)
+        {
+            customerDal.Add();
+        }
+    }
+}
--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -6,33 +6,69 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //InterfacesIntro();
+
+            //Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal[2]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal(),
+            };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlServerCustomerDal());
+            customerManager.Add(new OracleCustomerDal());
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+            manager.Add(new Customer { Id = 1, FirstName = "Engin", LastName = "Demiroğ", CustomerAddress = "Ankara" });
+            manager.Add(new Student { Id = 1, FirstName = "Derin", LastName = "Demiroğ", StudentNumber = "12345" });
         }
     }
     //abstract object, doesn't have a function alone
     interface IPerson//The operation to be implemented to all objects. IPerson contains common properties of Customer and Student.
     {
         int Id { get; set; }//Properties that Customer and Student classes must contain.Does not start with "public"
-        int FirstName { get; set; }//Properties that Customer and Student classes must contain.Does not start with "public"
-        int LastName { get; set; }//Properties that Customer and Student classes must contain.Does not start with "public"
+        string FirstName { get; set; }//Properties that Customer and Student classes must contain.Does not start with "public"
+        string LastName { get; set; }//Properties that Customer and Student classes must contain.Does not start with "public"
     }
     //concrete object
     class Customer : IPerson//Customer (= person) is like a subset of IPerson
     {
         public int Id { get ; set ; }//"public" versions of the properties in the IPerson interface
-        public int FirstName { get; set; }//"public" versions of the properties in the IPerson interface
-        public int LastName { get; set; }//"public" versions of the properties in the IPerson interface
-        public int CustomerAddress { get; set; }//customer specific property
-        public int CustomerNumber { get; set; }//customer specific property
+        public string FirstName { get; set; }//"public" versions of the properties in the IPerson interface
+        public string LastName { get; set; }//"public" versions of the properties in the IPerson interface
+        public string CustomerAddress { get; set; }//customer specific property
+        public string CustomerNumber { get; set; }//customer specific property
 
     }
     //concrete object
     class Student : IPerson//Student (= person) is like a subset of IPerson
     {
         public int Id { get; set; }//"public" versions of the properties in the IPerson interface
-        public int FirstName { get; set; }//"public" versions of the properties in the IPerson interface
-        public int LastName { get; set; }//"public" versions of the properties in the IPerson interface
-        public int StudentNumber { get; set; }//student specific property
+        public string FirstName { get; set; }//"public" versions of the properties in the IPerson interface
+        public string LastName { get; set; }//"public" versions of the properties in the IPerson interface
+        public string StudentNumber { get; set; }//student specific property
         public string Department { get; set; }//student specific property
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }      
     }
 }
